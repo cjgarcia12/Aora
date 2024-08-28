f13fdd893cd1bd9e64432570ceb1ba7598717852
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-08-27 20:29:52 -0400

    Day 1 of building the Aora app, just getting everything set up

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import { Tabs, Redirect } from 'expo-router';
+
+const AuthLayout = () => {
+  return (
+    <View>
+      <Text>AuthLayout
+    
+      </Text>
+    </View>
+  )
+}
+
+export default AuthLayout
